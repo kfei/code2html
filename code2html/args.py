89feafc6d986b13837dff0a043b8c1bddb07055c
kfei <kfei@kfei.net>
2013-09-12 12:13:55 +0800

    Add arguments parser and program scaffold

---
+++ code2html/args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+
+def get_args():
+    p = argparse.ArgumentParser()
+    p.add_argument("input", help="the path containing source code")   
+    p.add_argument("output", help="the path for saving output files")
+    p.add_argument("--color", help="the color scheme to use for syntax highlighting")
+
+    return p.parse_args()
---
+++ code2html/cli.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from args import get_args
+from subprocess import Popen, PIPE
+
+if __name__ == '__main__':
+    # Get the arguments passed by user
+    args = get_args()
+    
+    # TODO: Check whether Vim is available 
+
+    # TODO: Arguments validation
+
+    # TODO: Test the inupt/output directories
+
+    # TODO: Call Vim to do the conversion
