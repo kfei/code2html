35849633c3ce751445ffca92410001513d445ce9
kfei <kfei@kfei.net>
2013-09-18 19:13:53 +0800

    Check whether Vim is available

--- code2html/cli.py
+++ code2html/cli.py
@@ -9,7 +9,14 @@ if __name__ == '__main__':
     # Get the arguments passed by user
     args = get_args()
     
-    # TODO: Check whether Vim is available 
+    # Check whether Vim is available 
+    p1 = Popen(["vim", "--version"], stdout=PIPE)
+    p2 = Popen(["grep", "IMproved"], stdin=p1.stdout, stdout=PIPE)
+    vim_header = p2.communicate()[0].strip('\n')
+    if vim_header:
+        pass # Vim detected
+    else:
+        sys.exit(u'ERROR: Vim is not yet installed on this system, aborted.')
 
     # TODO: Arguments validation
 
